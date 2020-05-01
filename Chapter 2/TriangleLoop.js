ab2b805982c780db9de38c36e5b6515db4c231be
ConnorLacy <lacyconnor1409@gmail.com>
2020-05-01 16:24:47 -0400

    Restarting book

--- Chapter 2/TriangleLoop.js
+++
@@ -1,6 +0,0 @@
-let chars = ""
-
-for(let i = 0; i < 7; i++){
-    chars = chars + "#"
-    console.log(chars)
-}
--- Chapter 3/Minimum.js
+++
@@ -1,9 +0,0 @@
-/*
-    Write a function that takes two arguments and returns their minimum
-*/
-
-function getMin(a, b) {
-    return (a<b ? a : b);
-}
-
-console.log(getMin(10,30))
