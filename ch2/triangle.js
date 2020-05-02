266189d98ce56e3e2d4cee2a62a972d524f1f341
ConnorLacy <lacyconnor1409@gmail.com>
2020-05-02 13:50:19 -0400

    ex 2 done.

--- ch2/triangle.js
+++ ch2/triangle.js
@@ -1,11 +1,11 @@
 function triangle() {
-  for(let count=1; count<8; count++){
-    output = '#'
-    for(let anotherCount=1; anotherCount<count; anotherCount++){
-      output += '#'
+  for (let count = 1; count < 8; count++) {
+    output = "#";
+    for (let anotherCount = 1; anotherCount < count; anotherCount++) {
+      output += "#";
     }
-    console.log(output)
+    console.log(output);
   }
 }
 
-triangle()
+triangle();
