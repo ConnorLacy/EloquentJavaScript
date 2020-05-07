cacc2d3274ca84d77077b3169229747e54d7eefb
ConnorLacy <lacyconnor1409@gmail.com>
2020-05-07 13:03:03 -0400

    abstracted test of foreach and function binding

--- ch5/functions/my_foreach.mjs
+++ ch5/functions/my_foreach.mjs
@@ -1,14 +1,8 @@
 // This is a recreation of an existing HOF provided by the array prototype
 // which takes in a list and calls the provided action on that l
-function myForEach(list, action){
+export default function myForEach(list, action){
   for(let i=0; i<list.length; i++){
     let item = list[i]
     action(item)
   }
 }
-
-
-myForEach([1,2,3], l => console.log(l))
-
-
-export default myForEach;
---
+++ ch5/tests/test_foreach.mjs
@@ -0,0 +1,3 @@
+import myForEach from '../functions/my_foreach.mjs';
+
+myForEach([1,2,3], l => console.log(l))
